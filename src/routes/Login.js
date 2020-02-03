e16a7d66f36e30c5177931e216e86b6f66b99ccf
Sanskriti Lamsal <slamsal@go.olemiss.edu>
2020-02-03 13:52:19 -0600

    added login functionality

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,38 +1,56 @@
 import React, { Component } from "react";
 import ReactDom from "react";
+import { GoogleLogin } from "react-google-login";
+
+import FacebookLogin from "react-facebook-login";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "../style/Login.css";
+
 import Logo from "../asset/yoga.png";
-import Google from "../asset/Google.png"
-import Facebook from "../asset/Facebook.png"
+import Google from "../asset/Google.png";
+import Facebook from "../asset/Facebook.png";
+
+
+
+const responseGoogle = response => {
+  console.log(response);
+};
+
+const responseFacebook = response => {
+  console.log(response);
+};
 
 class Login extends Component {
   render() {
     return (
-      <div class="body">
-        <div class="container">
-          
-
-          <div class="row">
-            <div class="col-md-6 ">
-              <img src={Logo} alt="yog" class="w-100 image-logo" />
+      <div className="body">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 ">
+              <img src={Logo} alt="yog" className="w-100 image-logo" />
             </div>
-            <div class="col-md-3">
-              <div class="row align-items-center h-100">
-                <div class="col">
-                <button type="button" class="btn btn-labeled btn-login">
-                <span class="btn-label">
-                  <img class="login-btn-icon" src={Google}/>
-                  </span>Continue with Google &nbsp;&nbsp;  
-                  </button>
+            <div className="col-md-3">
+              <div className="row align-items-center h-100">
+                <div className="col">
+                  <GoogleLogin
+                    clientId="37866910395-f3925m928ti1vfve1qboea3o17r4a7st.apps.googleusercontent.com"
+                    buttonText="Continue with Google"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    cookiePolicy={"single_host_origin"}
+                  />
                   
-                  
-                    <br />
-                    <br />
-                    <button type="button" class="btn btn-labeled btn-login">
-                <span class="btn-label"><img class="login-btn-icon" src={Facebook}/></span>Continue with Facebook</button>
-            <br />  
-                 
+                  <br />
+                  <br />
+                  <FacebookLogin
+                    appId="555298271749129"
+                    autoLoad={true}
+                    fields="name,email,picture"
+                    
+
+                  />
+                  <br />
                 </div>
               </div>
             </div>
